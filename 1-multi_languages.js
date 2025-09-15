85c36b17794fa897c0702036a0b68d435221585f
iamnftlord <afolabigbenga1995@gmail.com>
2025-09-15 08:07:55 +0100

    Languages

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+const  firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const ThirdLine = 'JavaScript is amazing';
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(ThirdLine);
